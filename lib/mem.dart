f51029230a198991a4e8d11a21440bc1ce56419a
Diego Maldonado Acevedo <A328919@alumnos.uaslp.mx>
2025-09-18 09:05:12 -0600

    ExamenDiegoMaldonado

---
+++ lib/mem.dart
@@ -0,0 +1,13 @@
+class Mem extends StatefulWidget {
+  const Mem({super.key});
+
+  @override
+  State<Mem> createState() => _MemState();
+}
+
+class _MemState extends State<Mem> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
